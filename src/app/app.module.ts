5b2f80ed07636449b4efd068afd91e4887042d62
LawrBoni <31030984+Hikozak@users.noreply.github.com>
2017-08-24 16:59:05 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { AddStudentComponent } from './student-entry/src/app/student-entry/add-student/add-student.component';
+import { ListStudentComponent } from './student-entry/src/app/student-entry/list-student/list-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentEntryComponent,
+    AddStudentComponent,
+    ListStudentComponent
   ],
   imports: [
     BrowserModule,
